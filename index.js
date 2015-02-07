aaf643f558b0f2772a03ebd9941d2056412974d4
Thomas Leen <tl@thomasleen.com>
2015-02-06 21:16:59 -0500

    tweaked index

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ proxy = require('http-proxy');
 console.log(configuration);
 
 // work as a proxy
+var prx = proxy.createProxyServer({});
 
+var server = http.createServer(function(req, res){
+  // do it
+  prx.web(req, res, {target: configuration.target });
+});
 
-
+server.listen(configuration.port);
 
